b5e4771db7ffe3484896196f589740ea5a0f6686
Bubsa <bube.sim@gmail.com>
2025-05-19 20:34:09 +0200

    Class 09 - File System

---
+++ Class 09 File System/Class 09/Class 09/Program.cs
@@ -0,0 +1,104 @@
+﻿using Qinshift.Helpers;
+using System.Globalization;
+using System.IO;
+
+// ABSOLUTNI PATISHTA
+
+string studentsRepoFoldersFullPath = @"C:\Users\code\Documents\GitHub\advancedcsharp_homeworks\Class 09 File System\Class 09";
+string timeTrackingHomeworkFullPath = @"C:\Users\code\Documents\GitHub\advancedcsharp_homeworks\Class 09 File System\File System";
+
+// RELATIVNI PATISHTA
+
+string classReadmeRelativePath = @"..\..\..\..\..\ README.md";
+
+Console.WriteLine("========== DIRECTORY (Folder) ===============");
+
+// Zemi ja segashnata pateka na folderot
+
+string currentDirectory = Directory.GetCurrentDirectory();
+Console.WriteLine($"Current Directory {currentDirectory}");
+
+// Proveri dali postoi folder
+#region
+string testFolderPath = @"..\..\..\TestFolder";
+bool testFolderExists = Directory.Exists(testFolderPath);
+
+Console.WriteLine("The folder 'TestFolder' exists {0}", testFolderExists);
+
+if (!testFolderExists)
+{
+    Directory.CreateDirectory(testFolderPath);
+    Console.WriteLine("TestFolder created successfully");
+}
+else
+{
+    Console.WriteLine("TestFolder already exists");
+}
+#endregion
+
+// Brishenje na folder
+#region
+//if (Directory.Exists(testFolderPath))
+//{
+//    Directory.Delete(path: testFolderPath);
+//}
+#endregion
+
+Console.WriteLine("=================== FILE =====================");
+
+
+// PRAVENJE NA FAJL
+#region
+testFolderPath = @"..\..\..\TestFolder";
+string fileName = "example.txt";
+// string filePath = $@"{testFolderPath}\{fileName}";
+// Istoto ko ova gore ama na drug nacin
+string filePath = Path.Combine(testFolderPath, fileName);
+
+bool fileExists = File.Exists(filePath);
+
+if (!fileExists)
+{
+    File.Create(filePath).Close(); // Za da se izvrshi kodot na linija 80, mora da se pishe .Close() metodata za da moze da se izvrshi kodot pravilno
+}
+else
+{
+    Console.WriteLine("File already exists");
+}
+#endregion
+
+// BRISHENJE NA FAJL
+#region
+//if (File.Exists(filePath))
+//{
+//    File.Delete(filePath);
+//}
+#endregion
+
+// PISHUVANJE VO FAJL
+#region
+File.WriteAllText(filePath, "Hello From G3!");
+File.WriteAllText(filePath, "Hello from Qinshift"); // So ovaa linija na kod, tekstot se prepishuva i samo se stava ovoj text a ne dvata.
+#endregion
+
+
+// CHITANJE OD FAJL
+#region
+string fileContent = File.ReadAllText(filePath);
+Console.WriteLine(fileContent);
+#endregion
+
+// ZEMANJE INFO OD FAJL
+#region
+// INFORMACIJA IDE OVDE
+FileInfo fileInfo = new FileInfo(filePath);
+ConsoleHelper.WriteInColor("\nThe File's Info: ", ConsoleColor.Blue);
+Console.WriteLine("Full Name: " + fileInfo.FullName);
+Console.WriteLine("File Name: " + fileInfo.Name);
+Console.WriteLine("Extension: " + fileInfo.Extension);
+Console.WriteLine("Size: " + fileInfo.Length + " bytes");
+Console.WriteLine("Created On: " + fileInfo.CreationTime);
+Console.WriteLine("Last Modified: " + fileInfo.LastWriteTime);
+#endregion
+
+
---
+++ Class 09 File System/Class 09/FileStreams/Program.cs
@@ -0,0 +1,81 @@
+﻿using Qinshift.Helpers;
+
+ConsoleHelper.WriteInColor("\n================== FILE STREAMS ==================\n", ConsoleColor.Blue);
+
+// SETUP
+#region
+
+string folderPath = @"..\..\..\TestFiles";
+
+if (!Directory.Exists(folderPath))
+{
+    Directory.CreateDirectory(folderPath);
+}
+
+string fileName = "notes.txt";
+string fileFullPath = Path.Combine(folderPath, fileName);
+#endregion
+
+ConsoleHelper.WriteInColor("\n================== STREAM WRITER ==================\n", ConsoleColor.Cyan);
+
+#region
+using (StreamWriter streamWriter = new StreamWriter(fileFullPath)) // Using ti ovozmozuva da bide napraveno da ti go zavrshi kodot isto kako vo 62ra linija na prethodniot Program.cs
+ {
+    streamWriter.WriteLine("This is a simple note");
+    streamWriter.WriteLine("StreamWriter lets us write text line by line efficiently");
+    streamWriter.WriteLine("It also allows you to append or overwrite");
+ }
+
+//using (StreamWriter streamWriter = new StreamWriter(fileFullPath)) // Kolku pati i da pishuvas vaka sekogas ke se prepishuva toa sho go ima ovde 
+//{
+//    streamWriter.WriteLine("This is another note.");
+//    streamWriter.WriteLine("StreamWriter is awesome");
+
+//}
+
+using (StreamWriter streamWriter = new StreamWriter(fileFullPath, append: true)) // Se dodeka ne stavish append, tek togas si se dodava na tekstot kolku pati da ja uklucis aplikacijata
+{
+    streamWriter.WriteLine("This is another note.");
+    streamWriter.WriteLine("StreamWriter is awesome");
+}
+#endregion
+
+ConsoleHelper.WriteInColor("\n================== STREAM READER ==================\n", ConsoleColor.Cyan);
+
+#region
+
+try
+{
+    using (StreamReader streamReader = new StreamReader(fileFullPath))
+    {
+        ConsoleHelper.WriteInColor("Reading file content using StreamReader: \n", ConsoleColor.Yellow);
+
+        // Linija po linija kako da gi ischitash
+
+        //string firstLine = streamReader.ReadLine();
+        //Console.WriteLine("First line: " + firstLine);
+
+        //string secondLine = streamReader.ReadLine();
+        //Console.WriteLine("Second line: " + secondLine);
+
+        //string thirdLine = streamReader.ReadLine();
+        //Console.WriteLine("Third line: " + thirdLine);
+
+
+
+        // Istiot task ko pogore ama po dinamichki nachin
+
+        string line = streamReader.ReadLine();
+        while ( line != null)
+        {
+            Console.WriteLine(line);
+            line = streamReader.ReadLine();
+        }
+    }
+}
+catch (Exception ex)
+{
+    ConsoleHelper.WriteInColor("Error reading file:" + ex.Message, ConsoleColor.Red);
+}
+
+#endregion
