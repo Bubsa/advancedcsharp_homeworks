41ba768e5801fc6bf573b5dc67a469cf2186ba29
Bubsa <bube.sim@gmail.com>
2025-05-26 20:33:32 +0200

    Class 12 Database connecting
    Unfinished, last part missing

---
+++ Class 12/ConnectingWIthDatabase/ConnectingWIthDatabase/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdoNet.Models;
+using AdoNet.Services;
+
+Console.WriteLine("Hello, World!");
+// Za ova treba da se stisne desen klik na dependencies i da se instalira Microsoft.Data.SQLClient
+
+void PrintInColor(string text, ConsoleColor color = ConsoleColor.White)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(text);
+    Console.ResetColor();
+}
+
+void printStudents(List<Student> students)
+{
+    foreach (Student student in students)
+    {
+        Console.WriteLine(student);
+    }
+}
+
+string connectionString = "Server=.\\SQLEXPRESS;Database=SEDC_DEMO_SHARP;Trusted_Connection=True;Integrated Security=True;Encrypt=False;";
+
+StudentService studentService = new StudentService(connectionString);
+
+PrintInColor("\n========== All Students ===========", ConsoleColor.Cyan);
+var allStudents = studentService.GetAllStudents();
+printStudents(allStudents);
+
+
+PrintInColor("\n========== New Student ===========", ConsoleColor.Cyan);
+string firstName = Console.ReadLine();
+
+Student newStudent = new Student()
+{
+    FirstName = firstName,
+    LastName = "Doe",
+    DateOfBirth = new DateTime(2008,1,1),
+    EnrolledDate = new DateTime(2025,5,26),
+    Gender = 'M',
+    NationalIdNumber = 123123,
+    StudentCardNumber = "sc-85695"
+};
+
+studentService.InsertStudent(newStudent);
---
+++ Class 12/ConnectingWIthDatabase/ConnectingWIthDatabase/Services/StudentService.cs
@@ -0,0 +1,93 @@
+﻿using AdoNet.Models;
+using Microsoft.Data.SqlClient;
+
+namespace AdoNet.Services
+
+{
+    public class StudentService // Ovaa klasa ja pravime za da ne pishuvame vo Program.cs
+    {
+        private readonly string _connectionString;
+
+        public StudentService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            var students = new List<Student>();
+
+            // 1. Vospostavuvanje konekcija so Databazata
+            //SqlConnection connection = new SqlConnection(_connectionString);
+
+            //connection.Open(); // Ova e po nedobriot nachin na otvaranje konekcija so databaza. Po specificno so .Open ili .Close
+
+            //connection.Close();
+
+            // Ova e podobriot nachin odnosno koristenje USING
+
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                // 2. Pishi go query-to
+                string query = @"
+                    SELECT *
+                    FROM dbo.Student";
+
+                // 3. Kreiranje na SQL komanda
+                using SqlCommand command = new SqlCommand(query, sqlConnection);
+
+                // 4. Izvrshuvanje na SQL komandata
+                using SqlDataReader reader = command.ExecuteReader();
+
+                // 5. Chitanje na informacii od izvrsheniot query
+                while (reader.Read())
+                {
+
+                    // 6. Rachno mapiranje od vratenite koloni na Student objektot
+                    var student = new Student()
+                    {
+                        Id = reader.GetInt32(0),
+                        FirstName = reader.GetString(1),
+                        LastName = reader.GetString(2),
+                        DateOfBirth = reader.GetDateTime(3),
+                        EnrolledDate = reader.GetDateTime(4),
+                        Gender = reader.GetString(5)[0],
+                        NationalIdNumber = reader.GetInt64(6),
+                        StudentCardNumber = reader.GetString(7)
+                    };
+
+                    students.Add(student);
+                }
+
+            }
+
+
+            return students;
+        }
+
+        public void InsertStudent(Student student) 
+        {
+            // 1. Konekcija kon databazata
+            using (SqlConnection sqlConnection = new(_connectionString))
+            {
+                sqlConnection.Open();
+
+                // 2. Pishuvanje na SQL query
+                string query = $@"INSERT INTO dbo.Student (FirstName, LastName, DateOfBirth, EnrolledDate, Gender, NationalIdNumber, StudentCardNumber)
+VALUES('{student.FirstName}', '{student.LastName}', '{student.DateOfBirth:yyyy-MM-dd}', '{student.EnrolledDate:yyyy-MM-dd}', '{student.Gender}', {student.NationalIdNumber}, '{student.StudentCardNumber}')";
+                
+                // 3. Kreiranje na SQL komanda
+                using SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                // 4. Ova go izvrshuva query-to
+                sqlCommand.ExecuteNonQuery();
+            }
+
+
+        }
+
+    }
+        
+}
